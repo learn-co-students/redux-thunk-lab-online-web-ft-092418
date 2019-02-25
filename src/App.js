0fd5b744b9857fd4284bd886d69fa86a21c24b53
Peter-G-Stone <peter.stone.online@gmail.com>
2019-02-24 22:45:15 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap'
 import CatList from './CatList'
 import { connect } from 'react-redux'
-import fetchCats from './actions/catActions'
+import {fetchCats} from './actions/catActions'
 
 class App extends Component {   
 
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,7 +1,7 @@
-export default function fetchCats () {
+export function fetchCats () { //changing this off of export default to reg expert is what solved that final test (async actions). in App.js I then had to add the curly braces around the import fetchCats line
     return (dispatch) => { 
         dispatch({type: 'LOADING_CATS'})
-        return (fetch('http://localhost:4000/db'))
+        return fetch('http://localhost:4000/db')
             .then(resp => resp.json())
             .then(catData => dispatch({type: "FETCH_CATS", payload: catData.images}))
     }
